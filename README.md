2016e892d21d1070a8b6fcb2921c24a6512bb473
Christopher <christopher.loubassou@hotmail.fr>
2018-03-30 01:37:13 +0200

    Finish of markdown editor

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Markdown editor
 
-The goal of this project is to create a markdown editor with ReactJS
+The goal of this project is to create a markdown editor with ReactJS (with save in localStorage)
+
+Library marked is used in this project
+
+## Installation :
+
+```
+Npm install to install the dependence
+```
+
+And enjoy the editor
--- src/index.js
+++ src/index.js
@@ -1,10 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Import Css
+import './style/css/bootstrap.min.css';
+import './index.css';
+
+// Import personnal JS
+//import { sampleText } from './sampleText';
+
+// Marked.js
+import marked from 'marked';
+
 class App extends React.Component {
+
+    state = {
+        text: ''
+    };
+
+    componentWillMount() { 
+        const text = localStorage.getItem('text');
+
+        if(text){
+            this.setState({ text });
+        }
+    };
+
+    // Methode de REACT qui va se charger avant le rendu final
+    componentWillUpdate(nextProps, nextState) {
+        localStorage.setItem('text', nextState.text);
+    };
+
+    editText = (event) => {
+        const text = event.target.value;
+        this.setState({ text });
+    };
+
+    renderText = (text) => {
+        const renderText = marked(text, {sanitize: true});
+        return { __html: renderText };
+    };
+
     render(){
         return (
-            <div>Hello</div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <textarea 
+                        value={this.state.text} 
+                        rows="35" 
+                        className="form-control"
+                        onChange={(e) => this.editText(e)}
+                        >
+
+                        </textarea>
+                    </div>
+
+                <div className="col-sm-6">
+                    <div dangerouslySetInnerHTML={this.renderText(this.state.text)}/>
+                </div>
+
+                </div>
+            </div>
         );
     }
 }
